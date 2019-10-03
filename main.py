eb937ba12702ab583a1bc21745e75157e8d62198
Jon Stroop <jpstroop@gmail.com>
2019-10-03 16:27:28 -0400

    reorg output files and remove incomplete data from september

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from github_reporter import IssueReporter
 from github_reporter import GithubCommitter
 from github_reporter import HTMLReportRenderer
 from json import dump, load
-from os import environ
+from os import environ, makedirs, sep
 from os.path import abspath, dirname, exists, join
 from sys import exit, stderr
 import requests_cache
@@ -17,6 +17,10 @@ import requests_cache
 # TODO: html and json should be at the same path
 #    (html should be [tree of YYYY/MM/DD/index.html, json should be
 #     YYYY/MM/DD.json)
+# TODO: wrap all of the stuff not in main in a class. main() could even go in
+#  the class and then be aliased (if main is static) or instantiate the class
+#  and then call whatever it needs to call. All of these functions and HERE are
+#  getting annoying.
 # TODO: config file?
 #  - base title for HTML
 #  - number of days to keep (?)
@@ -47,22 +51,32 @@ def main():
     message = f'reports for {today.split("T")[0]}'
     committer.commit(file_paths, message)
 
+def make_date_dirs(iso_datetime):
+    renderer = HTMLReportRenderer()
+    date_dirs = iso_datetime.split("T")[0].split('-')
+    dir = join(HERE, 'docs', 'reports', *date_dirs)
+    makedirs(dir, exist_ok=True)
+    return dir
+
+
 def serialize_report(issue_report):
     json_path = render_as_json(issue_report)
     html_path = render_as_html(issue_report, 'issue_report_page.html.mako')
     return (json_path, html_path)
 
 def render_as_json(issue_report):
-    file_name = f'{issue_report["today"].split("T")[0]}.json'
-    file_path = join(HERE, 'docs', 'data', file_name)
+    html_dir = make_date_dirs(issue_report["today"])
+    dirs = html_dir.split(sep)
+    file_name = f'{dirs.pop()}.json'
+    file_path = f'{sep}{join(*dirs, file_name)}'
     with open(file_path, 'w') as f:
         dump(issue_report, f, indent=2, ensure_ascii=False)
     return file_path
 
 def render_as_html(issue_report, template):
     renderer = HTMLReportRenderer()
-    file_name = f'{issue_report["today"].split("T")[0]}.html'
-    file_path = join(HERE, 'docs', 'reports', file_name)
+    dir = make_date_dirs(issue_report["today"])
+    file_path = join(dir, 'index.html')
     with open(file_path, 'w') as f:
         f.write(renderer.render(template, r=issue_report))
     return file_path
--- templates/report_head.html.mako
+++ templates/report_head.html.mako
@@ -1,5 +1,5 @@
 <head>
     <meta charset="utf-8">
     <title>${title} for ${r['today']}</title>
-    <link rel = "stylesheet" type="text/css" href="../css/main.css" />
+    <link rel = "stylesheet" type="text/css" href="../../../../css/main.css" />
   </head>
