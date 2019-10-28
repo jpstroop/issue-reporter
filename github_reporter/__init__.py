6a52c534b18a77bb06a613f14fe6c0aa6752017e
Jon Stroop <jpstroop@gmail.com>
2019-10-27 21:31:01 -0400

    clean up main.py imports, run black

--- github_reporter/__init__.py
+++ github_reporter/__init__.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
 
-def timestamp(): # pragma: no cover
+def timestamp():  # pragma: no cover
     return datetime.now().isoformat()
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from datetime import datetime as dt
+from github_reporter import timestamp
 from github_reporter.app_setup import load_config, load_secrets
 from github_reporter.github_reporter import GithubReporter
 from sys import stderr
@@ -10,15 +10,15 @@ def main(event={}, context={}):
         secrets = load_secrets()
         config = load_config()
         if is_google_run:
-            print(f'{dt.now().isoformat()} - Google called me')
+            print(f'{timestamp()} - Google called me')
         if dump_to_stdout:
-            print(f'{dt.now().isoformat()} - Local run, will dump JSON to stdout')
+            print(f'{timestamp()} - Local run, will dump JSON to stdout')
         gh_reporter = GithubReporter(secrets, config, dump_to_stdout)
         commit_success = gh_reporter.run_report()
-        print(f'{dt.now().isoformat()} - Commit success: {commit_success}')
+        print(f'{timestamp()} - Commit success: {commit_success}')
         return True # Cloud Functions must return something
     except Exception as e:
-        print(f'{dt.now().isoformat()} - {e}', file=stderr)
+        print(f'{timestamp()} - {e}', file=stderr)
         return False
 
 if __name__ == '__main__':
